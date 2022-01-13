86fa7e8d8b7a0def266f154c8099aee79ff3abdf
daemon-shockley <daemon.shockley@gmail.com>
2022-01-13 12:32:22 -0800

    fixed logger configuration

--- api.js
+++ api.js
@@ -23,9 +23,8 @@ app.set('view engine', 'jade');
 var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' });
 
 // setup the logger
-app.use(morgan('combined', { stream: accessLogStream }));
+app.use(logger('combined', { stream: accessLogStream }));
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
