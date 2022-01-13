50b6cd7a3fde98ac4f3cc45294ab51e85961f686
daemon-shockley <daemon.shockley@gmail.com>
2022-01-13 12:59:34 -0800

    postgres connections should use ssl

--- api.js
+++ api.js
@@ -9,7 +9,10 @@ const { Client } = require('pg');
 var cors = require('cors')
 
 const credentials = {
-  connectionString: process.env.DATABASE_URL
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 };
 
 var testAPIRouter = require('./routes/testAPI');
